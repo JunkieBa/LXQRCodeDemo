8b72c6b5ce025925cc785ca248909f4a6ca370fd
lixi.bai <lx.bai@zuche.com>
2017-09-01 13:38:08 +0800

    二维码

---
+++ LXQRCodeDemo/LXQRCodeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXQRCodeDemo
+//
+//  Created by alisa on 2017/9/1.
+//  Copyright © 2017年 alisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
